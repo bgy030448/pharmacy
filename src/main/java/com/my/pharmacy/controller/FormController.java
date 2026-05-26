ebced7ab9d3a661b6df473982c779b59e0da98cd
bgy030448 <bgy030448@gmail.com>
2026-05-26 14:51:52 +0900

    feat: 주소로 검색하기 서비스구성 완료

--- src/main/java/com/my/pharmacy/controller/FormController.java
+++ src/main/java/com/my/pharmacy/controller/FormController.java
@@ -1,17 +1,40 @@
 package com.my.pharmacy.controller;
 
+import com.my.pharmacy.dto.DocumentDto;
+import com.my.pharmacy.dto.KakaoApiResponseDto;
+import com.my.pharmacy.service.KakaoAddressSearchService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+@RequiredArgsConstructor
+@Slf4j
 public class FormController {
+    private final KakaoAddressSearchService kakaoAddressSearchService;
+
     @GetMapping
     public String mainForm() {
         return "main";
     }
 
     @GetMapping("/output")
-    public String outputForm(){
+    public String outputForm() {
+        return "output";
+    }
+
+    @PostMapping("/search")
+    public String searchAddress(@RequestParam("address") String address) {
+        KakaoApiResponseDto kakaoApiResponseDto
+                = kakaoAddressSearchService.requestAddressSearch(address);
+        log.info("결과 : " + kakaoApiResponseDto);
+        // 결과 중 Documents만 빼서 dto에 저장
+        DocumentDto documentDto = kakaoApiResponseDto
+                .getDocumentList().get(0);
+        log.info("도큐먼트만 출력 : " + documentDto);
         return "output";
     }
 }
